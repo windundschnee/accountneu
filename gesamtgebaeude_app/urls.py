93e8767098a69fdaafcc530ee6f2664bb82cc0cd
Dein Name <du@example.com>
2019-09-28 18:54:52 +0200

    dfgdfgdfg

--- gesamtgebaeude_app/urls.py
+++ gesamtgebaeude_app/urls.py
@@ -6,6 +6,6 @@ app_name = 'gesamtgebaeude_app'
 
 urlpatterns = [
 
-    path('<slug:slug>/<int:my>/gesamtgebauede/<int:pk>/new',GesamtgebaeudeCreateView.as_view(), name='gesamtgebauede_create'),
+    #path('<slug:slug>/<int:my>/gesamtgebauede/<int:pk>/new',GesamtgebaeudeCreateView.as_view(), name='gesamtgebauede_create'),
 
 ]
