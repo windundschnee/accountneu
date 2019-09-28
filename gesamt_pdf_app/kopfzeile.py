4590e51082473aed00473ae02118b9c5309f7de8
Michi <m.schwaighofer1@gmx.at>
2019-09-28 18:14:41 +0200

    makemigrations

---
+++ gesamt_pdf_app/kopfzeile.py
@@ -0,0 +1,48 @@
+def kopfundfusszeile_einzeln(fd,arg_latex):
+    fd.write("\n"+r'\fancyhead[L]{\begin{tabular}{l r | l r}')
+    fd.write("\n"+r'	\textbf{Projekt}') # eintrag 1
+    fd.write("\n"+r' &  &')
+    fd.write("\n"+r'\textbf{Seite} & \thepage') # eintrag 2 Seitenzahl
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Bauteil}   & Wand') #eintrag 3 Bauteil
+    fd.write("\n"+r'&')
+    fd.write("\n"+r'\textbf{Datum} & \today') # eintrag 4 Datum
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Bauteilart} & Freistehende Wand') # eintrag 5 Bauteilart
+    fd.write("\n"+r'&')
+    fd.write("\n"+r'\textbf{Firma} & Musterman GMBH')  # eintrag 6 Bauteilart
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'&  &')#eintrag 7 nichts
+    fd.write("\n"+r'\textbf{Bearbeiter} & James Smith')#eintrag 8 Bearbeiter
+    fd.write("\n"+r'\end{tabular}}')
+
+def kopfundfusszeile(fd,arg_latex):
+    fd.write("\n"+r'\fancyhead[L]{\begin{tabular}{l r | l r}')
+    fd.write("\n"+r'	\textbf{Projekt}') # eintrag 1
+    fd.write("\n"+r' &  &')
+    fd.write("\n"+r'\textbf{Seite} & \thepage/\pageref{LastPage}') # eintrag 2 Seitenzahl
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Bauteil}   & Wand') #eintrag 3 Bauteil
+    fd.write("\n"+r'&')
+    fd.write("\n"+r'\textbf{Datum} & \today') # eintrag 4 Datum
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Bauteilart} & Freistehende Wand') # eintrag 5 Bauteilart
+    fd.write("\n"+r'&')
+    fd.write("\n"+r'\textbf{Firma} & Musterman GMBH')  # eintrag 6 Bauteilart
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'&  &')#eintrag 7 nichts
+    fd.write("\n"+r'\textbf{Bearbeiter} & James Smith')#eintrag 8 Bearbeiter
+    fd.write("\n"+r'\end{tabular}}')
+
+def kopfundfusszeile_1(fd,arg_latex):
+    fd.write("\n"+r'\fancyhead[L]{\begin{tabular}{l r | l r}')
+    fd.write("\n"+r'	\textbf{Projekt}') # eintrag 1
+    fd.write("\n"+r' & Beispielprojekt &')
+    fd.write("\n"+r'\textbf{Seite} & \thepage') # eintrag 2 Seitenzahl
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Firma}   & Musterman GMBH') #eintrag 3 Bauteil
+    fd.write("\n"+r'&')
+    fd.write("\n"+r'\textbf{Datum} & \today') # eintrag 4 Datum
+    fd.write("\n"+r'\\[-4pt]')
+    fd.write("\n"+r'\textbf{Bearbeiter} & James Smith') # eintrag 5 Bauteilart
+    fd.write("\n"+r'\end{tabular}}')
