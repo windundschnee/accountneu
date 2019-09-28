d71b482460af4a341c00a689dcfe0187f74ffe78
Dein Name <du@example.com>
2019-09-29 00:12:32 +0200

    fsdfsdf

--- gesamtgebaeude_app/urls.py
+++ gesamtgebaeude_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GesamtgebaeudeCreateView, GesamtgebaeudeDetailView
+from .views import GesamtgebaeudeCreateView
 
 
 app_name = 'gesamtgebaeude_app'
@@ -7,6 +7,6 @@ app_name = 'gesamtgebaeude_app'
 urlpatterns = [
 
     path('<slug:slug>/<int:my>/gesamtgebaeude/<int:pk>/new',GesamtgebaeudeCreateView.as_view(), name='gesamtgebaeude_create'),
-    path('<slug:slug>/<int:my>/gesamtgebaeude/<int:pk>', GesamtgebaeudeDetailView.as_view(), name='gesamtgebaeude_detail'),
+
 
 ]
