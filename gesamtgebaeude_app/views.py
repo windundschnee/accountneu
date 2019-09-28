a250cc2c4604a6d58cb3ae490015fbca642991dc
Dein Name <du@example.com>
2019-09-28 20:42:26 +0200

    gesamtgebäude

--- gesamtgebaeude_app/views.py
+++ gesamtgebaeude_app/views.py
@@ -5,11 +5,12 @@ from gesamt_pdf_app.models import GesamtPdf
 from django.forms.models import model_to_dict
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from .forms import GesamtgebauedeForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 class GesamtgebaeudeCreateView(LoginRequiredMixin, CreateView):
     model = Gesamtgebaeude
-    form_class = flachdaecherForm
+    form_class = GesamtgebauedeForm
 
 
     def form_valid(self, form):
@@ -21,7 +22,7 @@ class GesamtgebaeudeCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
-        context = super(FlachdachCreateView, self).get_context_data(**kwargs)
+        context = super(GesamtgebaeudeCreateView, self).get_context_data(**kwargs)
 
         context['pk'] = self.kwargs.get('my')
         context['slug'] = self.kwargs.get('slug')
