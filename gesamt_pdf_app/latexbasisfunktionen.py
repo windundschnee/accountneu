c3edf3e08c989db054512242959eb26e4268d093
Michi <m.schwaighofer1@gmx.at>
2019-09-28 17:53:52 +0200

    bilder reibung geändert

--- gesamt_pdf_app/latexbasisfunktionen.py
+++ gesamt_pdf_app/latexbasisfunktionen.py
@@ -128,12 +128,11 @@ def reibung_margin_tab(self,reibung_beruecksichtigen,fd):
         fd.write("\n"+r' $w_{fr}$ & Reibungsdruck')
         fd.write("\n"+r'\\')
 
-def reibung_margin_vernachlaessigen(self,reibung_beruecksichtigen,reibung_vernachlaessigt,fd):
+def reibung_margin_vernachlaessigen(self,reibung_vernachlaessigt,fd):
     windrichtung=['Norden und Süden', 'Osten und Westen']
-    if reibung_beruecksichtigen==True:
-        if all(element==True for element in reibung_vernachlaessigt )==True:
-            fd.write("\n"+r' Die Reibung darf für alle Windrichtungen vernachlassigt werden. \\')
-        else:
-            for ind, element in enumerate(reibung_vernachlaessigt):
-                if element == True:
-                    fd.write("\n"+r' Die Reibung darf für die Windrichtungen '+ windrichtung[ind]+r' vernachlassigt werden. \\')
+    if all(element==True for element in reibung_vernachlaessigt )==True:
+        fd.write("\n"+r' Die Reibung darf für alle Windrichtungen vernachlassigt werden. \\')
+    else:
+        for ind, element in enumerate(reibung_vernachlaessigt):
+            if element == True:
+                fd.write("\n"+r' Die Reibung darf für die Windrichtungen '+ windrichtung[ind]+r' vernachlassigt werden. \\')
