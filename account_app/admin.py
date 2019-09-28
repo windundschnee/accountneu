166ea0a8c9ec913a211adb825baedf94a071de41
Dein Name <du@example.com>
2019-09-28 18:52:42 +0200

    app

--- account_app/admin.py
+++ account_app/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(DjangoUserAdmin):
     #"""Define admin model for custom User model with no email field."""
 
     fieldsets = (
-        (None, {'fields': ('email', 'password' , 'company','is_pro', 'is_pro_date', 'is_pro_exired_date','verlaengerung_notwendig', 'is_free',
+        (None, {'fields': ('email', 'password' , 'company','logo_kopfzeile','is_pro', 'is_pro_date', 'is_pro_exired_date','verlaengerung_notwendig', 'is_free',
         'is_free_date', 'email_confirmed', 'allgEingaben_eingegeben',)}),
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
@@ -24,6 +24,6 @@ class UserAdmin(DjangoUserAdmin):
             'fields': ('email', 'password1', 'password2'),
         }),
     )
-    list_display = ('email', 'company', 'is_pro', 'is_pro_date', 'is_pro_exired_date', 'verlaengerung_notwendig', 'is_free', 'is_free_date', 'email_confirmed', 'allgEingaben_eingegeben')
+    list_display = ('email', 'company','logo_kopfzeile', 'is_pro', 'is_pro_date', 'is_pro_exired_date', 'verlaengerung_notwendig', 'is_free', 'is_free_date', 'email_confirmed', 'allgEingaben_eingegeben')
     search_fields = ('email', 'company')
     ordering = ('email',)
